6304f4cb2c547e09d0705168a746ed8ebf6aca0e
usha singh <ushasing215105@gmail.com>
2023-08-09 07:48:22 -0400

    add logo

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
   <header className="p-2 fixed shadow">
     <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
         <div class="container">
-            <a href="index.html"><img src="images/kweesha_capital_logo.png" class="img-fluid navbar-brand" alt="logo"></a>
+            <a href="index.html"><img src="images/kweesha_capital_logo.svg" class="img-fluid navbar-brand" alt="logo"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
